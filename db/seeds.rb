cc4503b45536a4f69ab55c46514e16ce1c7f2bdd
Clément <barceloneta08003@gmail.com>
2021-07-30 13:22:06 +0200

    starting on backend with bcrypt

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Jean", password: 'password')
+User.create(username: "Paul", password: 'password')
+User.create(username: "Luc", password: 'password')
+User.create(username: "Laurent", password: 'password')
+User.create(username: "Toto", password: 'password')
