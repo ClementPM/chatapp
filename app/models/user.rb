b043f8f0bf3eedc20906d7c48afb5ba19e0099b1
Clément <barceloneta08003@gmail.com>
2021-07-31 23:44:56 +0200

    user table created & linked to msg + refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true, length: { minimum: 4, maximum: 10}
+  validates :username, presence: true, uniqueness: { case_sensituve: false }, length: { minimum: 4, maximum: 10}
+  has_many :messages
   has_secure_password
 end
